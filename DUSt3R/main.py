62513c6ec703017e8fdb3a2a80e126a1fcbf02ba
1213tnals <1213tnals@naver.com>
2024-05-30 23:39:08 +0900

    main.py update

--- DUSt3R/main.py
+++ DUSt3R/main.py
@@ -17,7 +17,7 @@ def save_ply(pcd_array:torch.Tensor, filename):
     pcd.points = o3d.utility.Vector3dVector(pcd_array) 
  
     o3d.io.write_point_cloud(filename, pcd) 
-
+###################
 
 # 1. Get Images 
 image_dir = '../images'
@@ -50,6 +50,9 @@ scene = global_aligner(output, device=device, mode=GlobalAlignerMode.PointCloudO
 loss = scene.compute_global_alignment(init="mst", niter=niter, schedule=schedule, lr=lr)
 
 # retrieve useful values from scene:    Scene에는 image, focal, K(내부 파라미터), image_poses(외부 파라미터), point3d, mask 데이터가 들어있음
+# __init__.py의 global_aligner()가 있는 net = PointCloudOptimizer()에서 반환받고,
+# optimizer.py의 클래스는 PointCloudOptimizer(BasePCOptimizer)여서 BasePCOptimizer의 구조를 상속받음
+# 이때, BasePCOptimizer 클래스 내부에서 self.imgs, get_focals() 등이 있고, 함수는 PointCloudOptimizer에서 정의됨
 imgs = scene.imgs
 focals = scene.get_focals()
 poses = scene.get_im_poses()
@@ -71,16 +74,16 @@ torch.save(K, f'{save_dir}/intrinsics.pt')
 torch.save(depths, f'{save_dir}/depths.pt')
 torch.save(conf_vals, f'{save_dir}/conf_vals.pt')
 
-H0, W0, H1, W1 = *imgs[0].shape[:2], *imgs[1].shape[:2]      # Image Pair
-img0 = np.pad(imgs[0], ((0, max(H1 - H0, 0)), (0, 0), (0, 0)), 'constant', constant_values=0)
-img1 = np.pad(imgs[1], ((0, max(H0 - H1, 0)), (0, 0), (0, 0)), 'constant', constant_values=0) 
-img = np.concatenate((img0, img1), axis=1) 
+# H0, W0, H1, W1 = *imgs[0].shape[:2], *imgs[1].shape[:2]      # Image Pair
+# img0 = np.pad(imgs[0], ((0, max(H1 - H0, 0)), (0, 0), (0, 0)), 'constant', constant_values=0)
+# img1 = np.pad(imgs[1], ((0, max(H0 - H1, 0)), (0, 0), (0, 0)), 'constant', constant_values=0) 
+# img = np.concatenate((img0, img1), axis=1) 
 # plt.imshow(img)
 
 
 # 5. Make PLY file
-H,W = pts3d[0].shape[:2] 
-pcd_array = pts3d[0].view(H*W, 3).detach().cpu().numpy() 
+H,W = pts3d[0].shape[:2]    # pts3d[0] = (height, width, 3). pts3d[0].shape[:2] = 텐서의 1,2번째 차원의 크기 = 이미지의 높이(H)와 너비(W)
+pcd_array = pts3d[0].view(H*W, 3).detach().cpu().numpy()    # (height, width, 3) -> (height * width, 3). 각 행이 하나의 3D 포인트(3차원 좌표)를 나타냄.
 save_ply(pcd_array, 'input.ply') 
 
 
